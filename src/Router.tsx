fde5202c232f31c1fc995becf1092dec1b8da972
Samuel <samuelcardosodev@gmail.com>
2025-02-21 08:20:45 -0300

    feat: add route layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { History } from "./pages/History";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
-    return <Routes>
+  return (
+    <Routes>
+      {/* o path nesse primeiro Route, é "/" porque queremos aplicar o layout em todas as rotas. */}
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
-    </Routes>;
+      </Route>
+    </Routes>
+  );
 }
