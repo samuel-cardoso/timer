cb352ef0060e76e36f47e4e4f1b334e944be3328
Samuel <samuelcardosodev@gmail.com>
2025-02-21 07:58:02 -0300

    chore: add react-router-dom in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { ThemeProvider } from "styled-components";
-import Button from "./components/Button";
+import { BrowserRouter } from "react-router-dom";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
 
 export function App() {
   return (
     // O ThemeProvider me permite acessar o theme com a sintaxe ex: ${props => props.theme["green-500"]}.
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
--- src/components/_Button.tsx
+++ src/components/_Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles";
+import { ButtonContainer, ButtonVariant } from "./_Button.styles";
 interface ButtonProps {
   variant?: ButtonVariant;
 }
