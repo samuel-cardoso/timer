e8547801c225c421ae24e5fdd9b20891962a7eed
Samuel <samuelcardosodev@gmail.com>
2025-03-15 11:29:45 -0300

    feat: start creating history page

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme["gray-100"]};
+  }
+`;
+
+// Foi escolhido a div para fazer a tabel ter scroll no mobile, pois na tag table não é possível colcoar croll.
+export const HistoryList = styled.div`
+  flex: 1;
+  // isso vai fazer com que se crie um scroll se a tabela for maior do que o tamanho do container que eu tenho disponível para ela.
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    // vai mesclar as bordas de cada célula.
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background-color: ${(props) => props.theme["gray-600"]};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme["gray-100"]};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      // selecionando a primeira th.
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      // selecionando a última th.
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme["gray-700"]};
+      border-top: 4px solid ${(props) => props.theme["gray-800"]};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      // selecionando a primeira th.
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      // selecionando a última th.
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`;
